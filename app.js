e6e842ce66b88122b9d337849030c51065344213
Mmy26 <yu.b1994masamura@gmail.com>
2024-04-24 19:58:05 +0900

    バッグエンド情報隠蔽

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const app =  express();
 
+// EXpressの設定
 app.set("view engine", "ejs");
+app.disable("x-powered-by");
 
+// 静的情報
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
+// ルーティング
 app.use("/", require("./routes/index.js"));
-
+// アプリケーションの実行
 app.listen((PORT), ()=> {
   console.log(`Application listening at ${PORT}`);
 });
