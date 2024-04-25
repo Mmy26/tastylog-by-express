c01552240834469d0ccf51c8ac71228b647755ec
Mmy26 <yu.b1994masamura@gmail.com>
2024-04-25 20:42:34 +0900

    ログ出力の設定

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const PORT = process.env.PORT;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
+const applicationLogger = require("./lib/log/applicationlogger.js");
+const accessLogger = require("./lib/log/accessloger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const app =  express();
@@ -12,10 +14,14 @@ app.disable("x-powered-by");
 // 静的情報
 app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
 app.use("/public", express.static(path.join(__dirname, "/public")));
+// accessロガー
+app.use(accessLogger());
 // ルーティング
 app.use("/", require("./routes/index.js"));
+// アプリケーションのロガー
+app.use(applicationLogger());
 // アプリケーションの実行
 app.listen((PORT), ()=> {
-  logger.console.info(`Application listening at ${PORT}`);
+  logger.application.info(`Application listening at ${PORT}`);
 });
 
