73b0bd522ac068208b96c3a05a898e97ecefef1a
Mmy26 <yu.b1994masamura@gmail.com>
2024-05-03 17:21:43 +0900

    セッションの設定追加

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const appconfig = require("./config/application.config.js");
+const dbconfig = require("./config/mysql.config.js");
 const PORT = process.env.PORT;
 const path = require("path");
 const logger = require("./lib/log/logger.js");
@@ -6,6 +8,8 @@ const accessLogger = require("./lib/log/accessloger.js");
 const express = require("express");
 const favicon = require("serve-favicon");
 const cookie = require("cookie-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
 const app =  express();
 
 // EXpressの設定
@@ -26,6 +30,19 @@ app.use("/public", express.static(path.join(__dirname, "/public")));
 app.use(accessLogger());
 // ミドルミェアの設定
 app.use(cookie());
+app.use(session({
+  store: new MySQLStore({
+    host: dbconfig.HOST,
+    port: dbconfig.PORT,
+    user: dbconfig.USERNAME,
+    password: dbconfig.PASSWORD,
+    database: dbconfig.DATABASE
+  }),
+  secret: appconfig.security.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  name: "sid"
+}));
 app.use(express.urlencoded({ extended: true }));
 
 // ルーティング
@@ -37,7 +54,7 @@ app.use("/", require("./routes/index.js"));
 // アプリケーションのロガー
 app.use(applicationLogger());
 // アプリケーションの実行
-app.listen((PORT), ()=> {
+app.listen( appconfig.PORT, ()=> {
   logger.application.info(`Application listening at ${PORT}`);
 });
 
